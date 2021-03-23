46ac29ee47fb71a2ee4126e690239020317c26a4
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2021-03-23 23:55:40 +0100

    First CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Screen } from "./screen";
+import Menu from './menu/Menu';
+import Game from './game/Game';
 
 function App() {
+  const [screen, setScreen] = useState<Screen>("menu");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {screen === "menu" && <Menu/>}
+      {screen === "game" && <Game/>}
     </div>
   );
 }
---
+++ src/menu/Menu.tsx
@@ -0,0 +1,15 @@
+function Menu(){
+    return (<>
+        <div className="top">
+            <h1>The Wizard of Map</h1>
+        </div>
+        <div className="middle">
+            <a href="">Start Game</a>
+        </div>
+        <div className="bottom">
+            
+        </div>
+    </>);
+}
+
+export default Menu;
